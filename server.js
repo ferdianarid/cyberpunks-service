78ef964328df6c03296de097bf737a603c3b811c
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-05-14 18:16:32 +0700

    feature/create mongodb connection with mongoose done

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express")
 const apps = express()
-const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 const cors = require("cors")
+const dotenv = require("dotenv")
 const cookiesParser = require("cookie-parser")
 const routes = require("./src/routes")
 
-const Books = require("./src/models/Books")
-
 dotenv.config()
 
+// import models
+const Books = require("./src/models/Books")
+
+// define config port and callback
 const PORT = process.env.PORT
 const CALLBACK = () => `Server running on port ${PORT}`
 
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://localhost:27017/cyberpunks", { useUnifiedTopology: true }, () => "Database Connected")
+// mongoose create connection with mongodb
+mongoose.connect("mongodb://localhost:27017/cyberpunks", { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.error(err)
+    console.log("Connected to Database")
+})
 
 // credentials required and next js run in port 3000
 apps.use(cors({ credentials: true, origin: "http://localhost:3000" }))
@@ -22,4 +27,5 @@ apps.use(express.json())
 apps.use(cookiesParser())
 apps.use(routes)
 
+// server listen
 apps.listen(PORT, CALLBACK)
